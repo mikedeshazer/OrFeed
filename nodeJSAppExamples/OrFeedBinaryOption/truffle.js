93b14ba7d8e606156dbee1681cd8a6a2b511c91f
Nikhil <nikhilyadav3000@gmail.com>
2020-02-12 02:57:15 +0530

    binaryOption bounty

---
+++ nodeJSAppExamples/OrFeedBinaryOption/truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 9545,
+      network_id: '*' // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.4.26'
+    }
+  }
+}
