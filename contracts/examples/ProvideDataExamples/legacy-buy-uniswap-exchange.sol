19d35d7015fab6dbe55b17c70b03a39887d60a7b
mikedeshazer <guyonaplane57@gmail.com>
2020-05-10 21:40:33 +0900

    put the legacy buy- sell- kyber/uniswap code from hackathon into seperate smart contracts and updated oracle so that they are routed properly with the new protocol based on feedback that the tutorial lessons should still work even though a better way was found down the line

---
+++ contracts/examples/ProvideDataExamples/legacy-buy-uniswap-exchange.sol
@@ -0,0 +1,163 @@
+//Legacy contract to support the Buy-Uniswap-Exchange functionality from earlier tutorials
+//0x2c9b867ecf4a2f11f34c4d7c2163a201039fb407
+
+
+interface IUniswapFactory {
+    function createExchange(address token) external returns (address exchange);
+    function getExchange(address token) external view returns (address exchange);
+    function getToken(address exchange) external view returns (address token);
+    function getTokenWithId(uint256 tokenId) external view returns (address token);
+    function initializeFactory(address template) external;
+}
+
+
+interface OrFeedInterface {
+  function getExchangeRate ( string fromSymbol, string toSymbol, string venue, uint256 amount ) external view returns ( uint256 );
+  function getTokenDecimalCount ( address tokenAddress ) external view returns ( uint256 );
+  function getTokenAddress ( string symbol ) external view returns ( address );
+  function getSynthBytes32 ( string symbol ) external view returns ( bytes32 );
+  function getForexAddress ( string symbol ) external view returns ( address );
+}
+
+interface ERC20 {
+    function totalSupply() external view returns (uint supply);
+    function balanceOf(address _owner) external view returns (uint balance);
+    function transfer(address _to, uint _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
+    function approve(address _spender, uint _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint remaining);
+    function decimals() external view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+contract IUniswapExchange {
+    // Address of ERC20 token sold on this exchange
+    function tokenAddress() external view returns (address token);
+    // Address of Uniswap Factory
+    function factoryAddress() external view returns (address factory);
+    // Provide Liquidity
+    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable returns (uint256);
+    function removeLiquidity(uint256 amount, uint256 min_eth, uint256 min_tokens, uint256 deadline) external returns (uint256, uint256);
+    // Get Prices
+    function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
+    function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
+    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
+    function getTokenToEthOutputPrice(uint256 eth_bought) external view returns (uint256 tokens_sold);
+    // Trade ETH to ERC20
+    function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
+    function ethToTokenTransferInput(uint256 min_tokens, uint256 deadline, address recipient) external payable returns (uint256  tokens_bought);
+    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline) external payable returns (uint256  eth_sold);
+    function ethToTokenTransferOutput(uint256 tokens_bought, uint256 deadline, address recipient) external payable returns (uint256  eth_sold);
+    // Trade ERC20 to ETH
+    function tokenToEthSwapInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline) external returns (uint256  eth_bought);
+    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline, address recipient) external returns (uint256  eth_bought);
+    function tokenToEthSwapOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline) external returns (uint256  tokens_sold);
+    function tokenToEthTransferOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline, address recipient) external returns (uint256  tokens_sold);
+    // Trade ERC20 to ERC20
+    function tokenToTokenSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address token_addr) external returns (uint256  tokens_sold);
+    function tokenToTokenTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_sold);
+    // Trade ERC20 to Custom Pool
+    function tokenToExchangeSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address exchange_addr) external returns (uint256  tokens_sold);
+    function tokenToExchangeTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_sold);
+    // ERC20 comaptibility for liquidity tokens
+    bytes32 public name;
+    bytes32 public symbol;
+    uint256 public decimals;
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    // Never use
+    function setup(address token_addr) external;
+}
+
+
+
+  
+    // ERC20 Token Smart Contract
+    contract oracleInfo {
+        
+    
+      address owner; 
+      OrFeedInterface orfeed = OrFeedInterface(0x8316b082621cfedab95bf4a44a1d4b64a6ffc336);
+      address uniswapFactoryAddress = 0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95;
+    IUniswapFactory uniswapFactory = IUniswapFactory(uniswapFactoryAddress);
+
+
+      modifier onlyOwner() {
+            if (msg.sender != owner) {
+                throw;
+            }
+             _;
+        }
+        
+      constructor() public payable {
+            owner = msg.sender; 
+           
+        }
+        
+        
+    function getPriceFromOracle(string fromParam, string toParam, string  side, uint256 amount) public constant returns (uint256){  
+          
+        address sellToken = orfeed.getTokenAddress(fromParam);
+        address buyToken = orfeed.getTokenAddress(toParam);
+        
+        address exchangeAddressSell = uniswapFactory.getExchange(address(sellToken));
+        address exchangeAddressBuy = uniswapFactory.getExchange(address(buyToken));
+        
+        IUniswapExchange usi1 = IUniswapExchange(exchangeAddressSell);
+        IUniswapExchange usi2 = IUniswapExchange(exchangeAddressBuy);
+        
+       
+        uint256 resultingTokens;
+        uint256 ethBack;
+        
+        if(buyToken == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE){
+                resultingTokens = usi1.getEthToTokenOutputPrice(amount);
+                return resultingTokens;
+            }
+            if(buyToken == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE){
+                resultingTokens = usi2.getEthToTokenInputPrice(amount);
+                return resultingTokens;
+            }
+            
+             ethBack = usi2.getTokenToEthOutputPrice(amount);
+            resultingTokens = usi1.getEthToTokenOutputPrice(ethBack);
+            
+            //ethPrice1= usi2.getTokenToEthOutputPrice(amount);
+            
+            //ethPrice2 = usi1.getTokenToEthOutputPrice(amount);
+
+            //resultingTokens = ethPrice1/ethPrice2;
+            
+            return resultingTokens;
+
+
+          
+    }
+      
+      
+         
+     
+      
+      function changeOwner(address newOwner) onlyOwner returns(bool){
+          owner = newOwner;
+          return true;
+      }
+      
+     function withdrawBalance() onlyOwner returns(bool) {
+        uint amount = this.balance;
+        msg.sender.transfer(amount);
+        return true;
+
+    }
+    
+    
+    
+}
