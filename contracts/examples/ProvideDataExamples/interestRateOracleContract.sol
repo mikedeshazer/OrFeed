522b44069170327932b402ce16b26a5b5905f912
mikedeshazer <guyonaplane57@gmail.com>
2020-05-25 16:44:40 +0900

    updated interest rate ag contract and added mainnet ref

--- contracts/examples/ProvideDataExamples/interestRateOracleContract.sol
+++ contracts/examples/ProvideDataExamples/interestRateOracleContract.sol
@@ -1,3 +1,7 @@
+//Mainnet: https://etherscan.io/address/0xa2cae661463933c52e71983de5d33fb9624e0ff2#code
+
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -35,7 +39,7 @@ interface ERC20Detailed {
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function symbol() public view returns (string memory);
+    function symbol() external view returns (string memory);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -87,44 +91,53 @@ interface LendingPoolCore  {
     );
     function getReserveCurrentVariableBorrowRate(address _reserve) external view returns (uint256);
 }
-
 interface OrFeedInterface {
-    function getExchangeRate (string fromSymbol, string toSymbol, string venue, uint256 amount) external view returns (uint256);
-    function getTokenDecimalCount (address tokenAddress) external view returns (uint256);
-    function getTokenAddress (string symbol) external view returns (address);
-    function getSynthBytes32 (string symbol) external view returns (bytes32);
-    function getForexAddress (string symbol) external view returns (address);
+  function getExchangeRate ( string calldata fromSymbol, string calldata  toSymbol, string calldata venue, uint256 amount ) external view returns ( uint256 );
+  function getTokenDecimalCount ( address tokenAddress ) external view returns ( uint256 );
+  function getTokenAddress ( string calldata  symbol ) external view returns ( address );
+  function getSynthBytes32 ( string calldata  symbol ) external view returns ( bytes32 );
+  function getForexAddress ( string calldata symbol ) external view returns ( address );
+  function arb(address  fundsReturnToAddress,  address liquidityProviderContractAddress, string[] calldata   tokens,  uint256 amount, string[] calldata  exchanges) external payable returns (bool);
 }
 
 contract InterestRateOracleContract {
+    using SafeMath
+    for uint256;
+    
     struct PlatformSide {
         string platform;
         bool isBorrow;
     }
 
-    OrFeed orfeed = OrFeedInterface(0x8316b082621cfedab95bf4a44a1d4b64a6ffc336);
+   OrFeedInterface orfeed= OrFeedInterface(0x8316B082621CFedAB95bf4a44a1d4B64a6ffc336);
     DyDx dYdX = DyDx(0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e);
-    LendingPoolCore aave = LendingPoolCore(LendingPoolAddressesProvider(0x24a42fD28C976A61Df5D00D0599C34c4f90748c8).getLendingPoolCore());
+   // LendingPoolCore aave = LendingPoolCore(LendingPoolAddressesProvider(0x24a42fD28C976A61Df5D00D0599C34c4f90748c8).getLendingPoolCore());
+   //0x3dfd23a6c5e8bbcfc9581d2e864a68feb6a076d3
+   //0x5766067108e534419ce13f05899bc3e3f4344948
+
+   LendingPoolCore aave =  LendingPoolCore(0x3dfd23A6c5E8BbcFc9581d2E864a68feb6a076d3);
 
     uint256 constant ASCII_HYPHEN = 45;
     uint256 constant ASCII_ZERO = 48;
     uint256 constant ASCII_UPPERCASE_X = 88;
     uint256 constant ASCII_LOWERCASE_X = 120;
     uint256 constant DECIMAL = 10 ** 18;
-
+    address owner;
+    uint256 blocksPerYear = 2102400;
+    
     constructor() public payable {
-        owner = msg.sender;
+         owner = msg.sender;
     }
 
-    function getInterestRate(string fromParam, string, string venue, uint256 amount) public returns (uint256) {
-        string memory tokenSymbol = getTokenSymbol(fromParam);
-        PlatformSide memory platformSide = getPlatformSide(venue);
-        uint256 memory interestRate = getInterestRate(tokenSymbol, platformSide);
+    function getPriceFromOracle(string memory fromParam, string memory toParam, string memory venue, uint256 amount) public view returns (uint256) {
+        string memory tokenSymbol = fromParam;
+        PlatformSide memory platformSide = getPlatformSide(toParam, venue);
+        uint256  interestRate = getInterestRate(tokenSymbol, platformSide);
 
         return amount.mul(interestRate).div(DECIMAL);
     }
-
-    function getTokenSymbol(string input) internal returns (string memory) {
+/*
+    function getTokenSymbol(string memory input) internal returns (string memory) {
         bytes memory b = bytes(input);
 
         if (b.length < 3) {
@@ -135,72 +148,114 @@ contract InterestRateOracleContract {
             return input;
         }
     }
-
-    function getPlatformSide(string input) internal returns (PlatformSide memory) {
-        bytes memory b = bytes(input);
-        bytes memory platform;
-        bytes memory side;
-        bool afterHyphen = false;
-
-        for (uint i = 0; i < b.length; i++) {
-            if (b[i] == ASCII_HYPHEN) {
-                require(platform.length > 0, "Invalid platform string input");
-                afterHyphen = true;
-                continue;
-            }
-
-            if (afterHyphen) {
-                side.push(b[i]);
-            } else {
-                platform.push(b[i]);
-            }
+*/
+    function getPlatformSide(string memory whichPlatform, string memory doesBorrow) internal view returns (PlatformSide memory) {
+        bool isBorrowing;
+        if(equal("BORROW", doesBorrow)){
+            isBorrowing= true;
         }
+        else{
+            isBorrowing=false;
+        }
+        return PlatformSide(whichPlatform,isBorrowing);
+    }
+     
+function compare(string memory _a, string memory _b) internal view returns (int) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint minLength = a.length;
+        if (b.length < minLength) minLength = b.length;
+        //@todo unroll the loop into increments of 32 and do full 32 byte comparisons
+        for (uint i = 0; i < minLength; i ++)
+            if (a[i] < b[i])
+                return -1;
+            else if (a[i] > b[i])
+                return 1;
+        if (a.length < b.length)
+            return -1;
+        else if (a.length > b.length)
+            return 1;
+        else
+            return 0;
+    }
+    
+    function equal(string memory _a, string memory _b) internal view returns (bool) {
+        return compare(_a, _b) == 0;
+    }
+    
+    function indexOf(string memory _haystack, string memory _needle) internal returns (int)
+    {
+        bytes memory h = bytes(_haystack);
+        bytes memory n = bytes(_needle);
+        if(h.length < 1 || n.length < 1 || (n.length > h.length)) 
+            return -1;
+        else if(h.length > (2**128 -1)) // since we have to be able to return -1 (if the char isn't found or input error), this function must return an "int" type with a max length of (2^128 - 1)
+            return -1;                                  
+        else
+        {
+            uint subindex = 0;
+            for (uint i = 0; i < h.length; i ++)
+            {
+                if (h[i] == n[0]) // found the first char of b
+                {
+                    subindex = 1;
+                    while(subindex < n.length && (i + subindex) < h.length && h[i + subindex] == n[subindex]) // search until the chars don't match or until we reach the end of a or b
+                    {
+                        subindex++;
+                    }   
+                    if(subindex == n.length)
+                        return int(i);
+                }
+            }
+            return -1;
+        }   
+    }
+    
 
-        require(side.length > 0, "Invalid side string input");
-        return PlatformSide(string(platform), side == 'borrow');
+function concat(string memory _base, string memory _value) public view  returns (string memory) {
+       return string(abi.encodePacked(_base, _value));
     }
 
-    function getInterestRate(string symbol, PlatformSide platformSide) internal returns (uint256 memory) {
-        if (platformSide.platform == 'Compound') {
-            string memory platformToken;
-            if (symbol == 'BTC') {
-                platformToken = '0xC11b1268C1A384e55C48c2391d8d480264A3A7F4';
-            } else {
-                platformToken = abi.encodePacked('C', symbol);
-            }
 
-            Compound memory cToken = Compound(orfeed.getTokenAddress(platformToken));
+    function getInterestRate(string memory symbol, PlatformSide memory platformSide) public view returns (uint256 ) {
+        if (equal(platformSide.platform, "COMPOUND")) {
+            string memory platformToken;
+           
+            platformToken = concat("C", symbol);
+           
+
+            Compound  cToken = Compound(orfeed.getTokenAddress(platformToken));
 
             if (platformSide.isBorrow) {
-                return cToken.borrowRatePerBlock().mul(cToken.interestRateModel.blocksPerYear);
+                return cToken.borrowRatePerBlock().mul(blocksPerYear);
             } else {
-                return cToken.supplyRatePerBlock().mul(cToken.interestRateModel.blocksPerYear);
+               return cToken.supplyRatePerBlock().mul(blocksPerYear);
             }
-        } else if (platformSide.platform == 'Fulcrum') {
+        } else if (equal(platformSide.platform , 'FULCRUM')) {
             string memory platformToken;
-            if (symbol == 'BTC') {
-                platformToken = '0xba9262578efef8b3aff7f60cd629d6cc8859c8b5';
-            } else {
-                platformToken = abi.encodePacked('I', symbol);
-            }
+           
+                platformToken = concat('I', symbol);
+           
 
-            Fulcrum memory iToken = Fulcrum(orfeed.getTokenAddress(platformToken));
+            Fulcrum  iToken = Fulcrum(orfeed.getTokenAddress(platformToken));
 
             if (platformSide.isBorrow) {
-                return iToken.borrowInterestRate().div(100);
+                return iToken.borrowInterestRate().div(1000);
             } else {
-                return iToken.supplyInterestRate().div(100);
+                return iToken.supplyInterestRate().div(1000);
             }
-        } else if (platformSide.platform == 'dYdX') {
-            uint memory marketId;
-            if (symbol == 'ETH') {
+        } else if (equal(platformSide.platform , 'DYDX')) {
+            uint  marketId;
+            if (equal(symbol, "ETH")) {
                 marketId = 0;
-            } else if (symbol == 'DAI') {
+            } else if (equal(symbol, "SAI")) {
                 marketId = 1;
-            } else if (symbol == 'USDC') {
+            } else if (equal(symbol, "USDC")) {
                 marketId = 2;
+            } else if (equal(symbol, "DAI")) {
+                marketId = 3;
             } else {
-                require(false, abi.encodePacked('dYdX does not have market data for ', symbol));
+                require(false, concat('dYdX does not have market data for ', symbol));
             }
 
             uint256 rate = dYdX.getMarketInterestRate(marketId).value;
@@ -214,18 +269,16 @@ contract InterestRateOracleContract {
             uint256 supply = dYdX.getMarketTotalPar(marketId).supply;
             uint256 usage = (borrow * DECIMAL) / supply;
             return (((aprBorrow * usage) / DECIMAL) * dYdX.getEarningsRate().value) / DECIMAL;
-        } else if (platformSide.platform == 'Aave') {
+        } else if (equal(platformSide.platform ,'AAVE')) {
             string memory platformToken;
-            if (symbol == 'BTC') {
-                platformToken = '0xFC4B8ED459e00e5400be803A9BB3954234FD50e3';
-            } else {
-                platformToken = abi.encodePacked('A', symbol);
-            }
+         
+               // platformToken = concat('A', symbol);
+            platformToken= symbol;
 
             if (platformSide.isBorrow) {
-                return aave.getReserveCurrentVariableBorrowRate(platformToken).div(1e9);
+                return aave.getReserveCurrentVariableBorrowRate(orfeed.getTokenAddress(platformToken)).div(1e9);
             }
-            return aave.getReserveCurrentLiquidityRate(platformToken).div(1e9);
+            return aave.getReserveCurrentLiquidityRate(orfeed.getTokenAddress(platformToken)).div(1e9);
         } else {
             require(false, 'Platform not supported');
         }
